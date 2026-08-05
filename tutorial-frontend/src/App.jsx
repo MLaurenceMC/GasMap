e6993b80792e6233f11696ab20a2f44c01a12abf
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-05 16:00:08 +0800

    daisy theme and navbar

--- tutorial-frontend/src/App.jsx
+++ tutorial-frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
+      <html data-theme="dracula"></html>
+      <Navbar />
       <h1 class="text-3xl font-bold underline">
         Hello world!
       </h1>
