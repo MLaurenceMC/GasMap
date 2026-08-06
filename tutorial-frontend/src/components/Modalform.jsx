5156b6637039db77eb8d3d61a88c32de0734edc7
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-06 21:25:45 +0800

    eod, error: crash when input is selected

--- tutorial-frontend/src/components/Modalform.jsx
+++ tutorial-frontend/src/components/Modalform.jsx
@@ -2,17 +2,49 @@ export default function ModalForm({ isOpen, onClose, mode, OnSubmit }) {
   return (
     <>
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
-      <dialog id="my_modal_3" className="modal"
-      open={isOpen}>
-        <div className="modal-box">
-          <h3 className="font-bold text-lg py-4">{mode === 'edit' ? 'Edit Item' : 'Item Details'}</h3>
+      <dialog id="my_modal_3" className="modal" open={isOpen}>
+        <div className="modal-box flex w-auto flex-col flex-wrap items-start gap-2">
+          <h3 className="text-lg font-bold">
+            {mode === 'edit' ? 'Edit Item' : 'Item Details'}
+          </h3>
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2" onClick={onClose}>
+            <label className="input input-bordered flex w-full items-center gap-2">
+              Name
+              <input type="text" className="grow" placeholder="Product Name" />
+              {/* <span className="badge badge-neutral badge-xs">Optional</span> */}
+            </label>
+            <div className="flex justify-between">
+              <label className="input input-bordered flex items-center gap-2">
+                Quantity
+                <input type="number" className="grow" placeholder="20" />
+              </label>
+              <label className="input input-bordered flex items-center gap-2">
+                Price
+                <input type="number" className="grow" placeholder="5.99" />
+              </label>
+            </div>
+            <label className="input input-bordered flex w-full items-center gap-2">
+              Location
+              <input type="text" className="grow" placeholder="Philippines" />
+            </label>
+            <select
+              defaultValue="Status"
+              className="select select-bordered w-full"
+            >
+              <option>Active</option>
+              <option>Inactive</option>
+            </select>
+            <button
+              onClick={onClose}
+              className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2"
+            >
               ✕
             </button>
-            <button className="btn btn-success">{mode === 'edit' ? 'Save Changes' : 'Add Item'}</button>
           </form>
+          <button className="btn btn-success">
+            {mode === 'edit' ? 'Save Changes' : 'Add Item'}
+          </button>
         </div>
       </dialog>
     </>
