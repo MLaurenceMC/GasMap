4380a93c89f37acd18e9dd2395ca42680e1463f3
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-17 16:35:16 +0800

    [1 hr mark]

--- tutorial-backend/src/controllers/clientControllers.js
+++ tutorial-backend/src/controllers/clientControllers.js
@@ -1,1 +1,11 @@
-import { query } from "../db.js";
+import * as clientService from '../services/clientServices.js';
+
+export const getClients = async (req, res) => {
+    try {
+        const clients = await clientService.getClients();
+        res.status(200).json(clients);
+    } catch (error) {
+        console.error('Error fetching clients:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
