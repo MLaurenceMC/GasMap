6579d3434a943a5196df06bfa7aed8cc07f503fe
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-17 16:07:19 +0800

    [54:50]

--- tutorial-backend/src/index.js
+++ tutorial-backend/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
-// Define your routes here
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello Backend');
 });
