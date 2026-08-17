e11210ba16ca9fa767057660bbe69c790ad97c76
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-17 17:22:42 +0800

    [1.03.23]

--- tutorial-backend/src/services/clientServices.js
+++ tutorial-backend/src/services/clientServices.js
@@ -1,6 +1,17 @@
 import { query } from "../db.js";
 
-export const getClients = async() => {
-    const { rows } = await query("SELECT * FROM clients_tb");
-    return rows;
-}
+export const getClients = async () => {
+  const { rows } = await query("SELECT * FROM clients_tb");
+  return rows;
+};
+
+export const createClient = async (clientData) => {
+  const { name, email, job, rate, isActive } = clientData;
+  const { rows } = await query(
+    `INSERT INTO clients_tb (name, email, job, rate, isActive) 
+     VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [name, email, job, rate, isActive],
+  );
+
+  return rows[0];
+};
