bab161f0473d4096739e0bf64ad9ab8b5bc01174
Laurence Castillo <143578936+MLaurenceMC@users.noreply.github.com>
2026-08-21 12:51:28 +0800

    feat: add NavBar and SideBar

--- GasMap/frontend/src/App.jsx
+++ GasMap/frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 
+import MapBox from "./components/MapBox";
+import ModalForm from "./components/ModalForm";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+
 function App() {
+
   return (
     <>
-      <div data-theme="dracula">Test</div>
+      <div data-theme="dracula">
+        <NavBar></NavBar>
+        {/* <MapBox></MapBox> */}
+        <SideBar></SideBar>
+        {/* <ModalForm></ModalForm> */}
+      </div>
     </>
   );
 }
